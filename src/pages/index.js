787185602876f97368a457d0ee677e3449099f1a
BulboGC <gabrielshot1@gmail.com>
2023-06-13 11:18:12 -0300

    alterado index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,37 @@
-import AddForm from "../components/index/Addform";
+import ClienteLista from "../components/index/ClienteLista";
 import TabNav from "../components/index/Tabnav";
 import { Tab, Row, Col } from "react-bootstrap";
 import React, { useContext } from "react";
 import ThemeContext from "../contexts/ThemeContext";
 
+export async function getStaticProps(context) {
+  const res = await fetch("http://localhost:4000/ficha");
+  const clientes = await res.json();
 
-function App() {
+  return {
+    props: {
+      clientes,
+    },
+    revalidate: 30,
+  };
+}
+
+function App({ clientes }) {
   const { theme } = useContext(ThemeContext);
+  const data = clientes;
+
 
   return (
-    <Tab.Container id="tabid">
+    <Tab.Container>
       <Row className={`fundo ${theme}`}>
         <Col xs={1} style={{ maxWidth: "100px", minWidth: "100px" }}>
-          <TabNav />
+          <TabNav at={true} />
         </Col>
         <Col>
-          <AddForm eventKey="Form" />
+          {data &&
+            data.map((client, index) => {
+              return <ClienteLista name={client.name} key={index} />;
+            })}
         </Col>
       </Row>
     </Tab.Container>
